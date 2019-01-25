27a3eb5fbcf02e3534b0f7eaefa378fd35676fa8
kronicle114 <trisha.aguinaldo@gmail.com>
2019-01-25 08:38:26 -0500

    added cuter cat & dog data

--- index.js
+++ index.js
@@ -31,21 +31,21 @@ const cat = [{
   breed: 'Bengal',
   story: 'Thrown on the street'
 }, {
-  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-  imageDescription: '2nd cat',
-  name: 'Fluffy',
+  imageURL:'https://images.unsplash.com/photo-1494256997604-768d1f608cac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60', 
+  imageDescription: 'Brown Scottish fold cat',
+  name: 'Ymir',
   sex: 'Female',
-  age: 2,
-  breed: 'Bengal',
-  story: 'Thrown on the street'
+  age: 3,
+  breed: 'Scottish Fold',
+  story: 'Owner moved away'
 }, {
-  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
-  imageDescription: '3rd cat',
-  name: 'Fluffy',
+  imageURL:' https://unsplash.com/photos/b079C-_tUbM', 
+  imageDescription: 'Orange tabby cat yawning',
+  name: 'Melisandre',
   sex: 'Female',
-  age: 2,
-  breed: 'Bengal',
-  story: 'Thrown on the street'
+  age: 10,
+  breed: 'Tabby',
+  story: 'Too sleepy for her own good'
 }
 ];
 
@@ -58,21 +58,21 @@ const dog = [{
   breed: 'Golden Retriever',
   story: 'Owner Passed away'
 },{
-  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-  imageDescription: '2nd dog',
-  name: 'Zeus',
+  imageURL: 'https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60',
+  imageDescription: 'A handsome golden retrievier puppy with red collar',
+  name: 'Brutus',
   sex: 'Male',
-  age: 3,
+  age: 1,
   breed: 'Golden Retriever',
-  story: 'Owner Passed away'
+  story: 'Owner moved away'
 },{
-  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-  imageDescription: '3rd dog',
-  name: 'Zeus',
+  imageURL: 'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=60',
+  imageDescription: 'A very hipster black pug',
+  name: 'Shakespeare',
   sex: 'Male',
-  age: 3,
-  breed: 'Golden Retriever',
-  story: 'Owner Passed away'
+  age: 4,
+  breed: 'Pug',
+  story: 'Too hipster for his own good'
 }
 ];
 
@@ -81,7 +81,7 @@ app.get('/api/cat', (req, res, next) => {
 });
 
 app.get('/api/dog', (req, res, next) => {
-  return res.json(dog);
+  return res.json(dog[0]);
 });
 
 app.delete('/api/dog', (req, res, next) => {
